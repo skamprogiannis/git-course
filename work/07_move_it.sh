684e509ac91211777e5a94c9f33171909de230be
skamprogiannis <boot.stefan.os@proton.me>
2026-03-12 14:39:19 +0200

    fix(work): close heredoc and quote EOF delimiter in move it script

--- work/07_move_it.sh
+++ work/07_move_it.sh
@@ -3,8 +3,9 @@ mkdir lib
 git mv hello.sh lib/
 git commit -m "chore: move hello.sh to lib directory"
 
-cat <<EOF > Makefile
+cat <<'EOF' > Makefile
 TARGET="lib/hello.sh"
 
 run:
 	bash ${TARGET}
+EOF
